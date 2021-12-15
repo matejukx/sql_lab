a97bcb9f21cadc3e84d7c047db90306fcd3a200a
brzzw5k <74360157+brzzw5k@users.noreply.github.com>
2021-12-15 12:19:49 +0100

    Add month-year dates loading

--- ladowanie_dat.sql
+++ ladowanie_dat.sql
@@ -5,23 +5,23 @@ go
 -- Step a: Declare variables use in processing
 Declare @StartDate date; 
 Declare @EndDate date;
+Declare @CurrentSeason varchar(6);
 
 -- Step b:  Fill the variable with values for the range of years needed
 SELECT @StartDate = '2013-01-01', @EndDate = '2025-12-31';
 
--- Step c:  Use a while loop to add dates to the table
+-- Step c: Insert NULL date
+Insert Into [dbo].t_date
+		( DateYear, DateMonth, DateDay, Season )
+		Values ( NULL, NULL, NULL, NULL);
+
+-- Step d:  Use a while loop to add dates to the table
 Declare @DateInProcess datetime = @StartDate;
 
 While @DateInProcess <= @EndDate
 	Begin
-	--Add a row into the date dimension table for this date
-		Insert Into [dbo].t_date
-		( DateYear, DateMonth, DateDay, Season )
-		Values ( 
-		    Cast( Year(@DateInProcess) as varchar(4)) -- year
-		  , Cast( Month(@DateInProcess) as int) -- month
-		  , Cast( DATEPART(day, @DateInProcess) as int) -- day of month
-		  , CASE 
+
+		SET @CurrentSeason = CASE 
 				WHEN DATEPART(month, @DateInProcess) IN (3) AND DATEPART(day, @DateInProcess)>=20 THEN 'wiosna'
 				WHEN DATEPART(month, @DateInProcess) IN (4, 5) THEN 'wiosna'
 				WHEN DATEPART(month, @DateInProcess) IN (6) AND DATEPART(day, @DateInProcess)<21 THEN 'wiosna'
@@ -34,9 +34,52 @@ While @DateInProcess <= @EndDate
 				WHEN DATEPART(month, @DateInProcess) IN (12) AND DATEPART(day, @DateInProcess)>=21 THEN 'zima'
 				WHEN DATEPART(month, @DateInProcess) IN (3) AND DATEPART(day, @DateInProcess)<20 THEN 'zima'
 				ELSE 'zima'
-			END -- season
-		);  
+			END 
+		--Add a row into the date dimension table for this date
+		Insert Into [dbo].t_date
+		( DateYear, DateMonth, DateDay, Season )
+		Values ( 
+		    Cast( Year(@DateInProcess) as varchar(4)) -- year
+		  , Cast( Month(@DateInProcess) as int) -- month
+		  , Cast( DATEPART(day, @DateInProcess) as int) -- day of month
+		  , @CurrentSeason
+		);
+
 		-- Add a day and loop again
 		Set @DateInProcess = DateAdd(d, 1, @DateInProcess);
 	End
-go
+
+	-- Step e:  Use a while loop to add month-year dates to the table
+Set @DateInProcess = @StartDate;
+
+While @DateInProcess <= @EndDate
+	Begin
+
+		SET @CurrentSeason = CASE 
+				WHEN DATEPART(month, @DateInProcess) IN (3) AND DATEPART(day, @DateInProcess)>=20 THEN 'wiosna'
+				WHEN DATEPART(month, @DateInProcess) IN (4, 5) THEN 'wiosna'
+				WHEN DATEPART(month, @DateInProcess) IN (6) AND DATEPART(day, @DateInProcess)<21 THEN 'wiosna'
+				WHEN DATEPART(month, @DateInProcess) IN (6) AND DATEPART(day, @DateInProcess)>=21 THEN 'lato'
+				WHEN DATEPART(month, @DateInProcess) IN (7, 8) THEN 'lato'
+				WHEN DATEPART(month, @DateInProcess) IN (9) AND DATEPART(day, @DateInProcess)<23 THEN 'lato'
+				WHEN DATEPART(month, @DateInProcess) IN (9) AND DATEPART(day, @DateInProcess)>=21 THEN 'jesień'
+				WHEN DATEPART(month, @DateInProcess) IN (10, 11) THEN 'jesień'
+				WHEN DATEPART(month, @DateInProcess) IN (12) AND DATEPART(day, @DateInProcess)<21 THEN 'jesień'
+				WHEN DATEPART(month, @DateInProcess) IN (12) AND DATEPART(day, @DateInProcess)>=21 THEN 'zima'
+				WHEN DATEPART(month, @DateInProcess) IN (3) AND DATEPART(day, @DateInProcess)<20 THEN 'zima'
+				ELSE 'zima'
+			END 
+		--Add a row into the date dimension table for this month-year date
+		Insert Into [dbo].t_date
+		( DateYear, DateMonth, DateDay, Season )
+		Values ( 
+		    Cast( Year(@DateInProcess) as varchar(4)) -- year
+		  , Cast( Month(@DateInProcess) as int) -- month
+		  , NULL
+		  , @CurrentSeason
+		);
+
+		-- Add a month and loop again
+		Set @DateInProcess = DateAdd(m, 1, @DateInProcess);
+	End
+Go
