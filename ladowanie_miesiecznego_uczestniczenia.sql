56ff91bfe66c7d7dac8f2d206e07a56f16c38dac
brzzw5k <74360157+brzzw5k@users.noreply.github.com>
2021-12-15 22:52:48 +0100

    Update ladowanie_miesiecznego_uczestniczenia.sql

--- ladowanie_miesiecznego_uczestniczenia.sql
+++ ladowanie_miesiecznego_uczestniczenia.sql
@@ -1,104 +1,11 @@
-﻿/*Declare @StartDate date; 
-Declare @EndDate date;
+IF (object_id('nullEmployee') is not null) DROP VIEW nullEmployee
+GO
 
-SELECT @StartDate = '2013-01-01', @EndDate = '2025-12-31';
-
-Declare @DateInProcess datetime = @StartDate;
-
-While @DateInProcess <= @EndDate
-	Begin
-		SELECT student.StudentID, InstructorID, LecturerID, DrivesDone
-		FROM 
-		(
-			SELECT
-				id_pesel_meetingId.ID AS [StudentID],
-				COUNT(m.PK_Id) AS [DrivesDone]
-			FROM
-			(
-				SELECT ID, PESEL, FK_Meeting_Id FROM 
-				(
-					SELECT PK_PESEL
-					FROM [DrivingSchool16].[dbo].Student
-				) s
-				JOIN 
-				(
-					SELECT ID, PESEL
-					FROM [hurtownia].[dbo].t_student 
-				) ts
-				ON s.PK_PESEL = ts.PESEL
-				JOIN 
-				(
-					SELECT *
-					FROM [DrivingSchool16].[dbo].Participation
-				) p
-				ON s.PK_PESEL = p.FK_Student_PESEL
-				GROUP BY ts.ID, ts.PESEL, p.FK_Meeting_Id
-			) id_pesel_meetingId
-			JOIN
-			(
-				SELECT PK_Id, [Type], Begin_date, End_date
-				FROM [DrivingSchool16].[dbo].Meeting
-			) m
-			ON id_pesel_meetingId.FK_Meeting_Id = m.PK_Id 
-				AND [Type] = 'Lecture' /* 'Practice' */
-				AND DATEPART(MONTH, @DateInProcess) = DATEPART(MONTH, m.Begin_date)
-				AND DATEPART(YEAR, @DateInProcess) = DATEPART(YEAR, m.Begin_date)
-			GROUP BY id_pesel_meetingId.ID
-		) student
-		LEFT JOIN 
-		(
-			SELECT
-				ts.ID AS [StudentID],
-				e.ID AS [InstructorID]
-			FROM
-				[DrivingSchool16].[dbo].Student s
-				JOIN [hurtownia].[dbo].t_student ts
-				ON s.PK_PESEL = ts.PESEL
-				JOIN [DrivingSchool16].[dbo].Participation p
-				ON s.PK_PESEL = p.FK_Student_PESEL
-				JOIN [DrivingSchool16].[dbo].Meeting m
-				ON p.FK_Meeting_Id = m.PK_Id 
-					AND m.Type = 'Practice'
-					AND DATEPART(MONTH, @DateInProcess) = DATEPART(MONTH, m.Begin_date)
-					AND DATEPART(YEAR, @DateInProcess) = DATEPART(YEAR, m.Begin_date)
-				JOIN [hurtownia].[dbo].t_employee e
-				ON m.FK_Employee_PESEL = e.PESEL
-		) instructor
-		ON student.StudentID = instructor.StudentID
-		LEFT JOIN
-		(
-			SELECT
-				ts.ID AS [StudentID],
-				e.ID AS [LecturerID]
-			FROM
-				[DrivingSchool16].[dbo].Student s
-				JOIN [hurtownia].[dbo].t_student ts
-				ON s.PK_PESEL = ts.PESEL
-				JOIN [DrivingSchool16].[dbo].Participation p
-				ON s.PK_PESEL = p.FK_Student_PESEL
-				JOIN [DrivingSchool16].[dbo].Meeting m
-				ON p.FK_Meeting_Id = m.PK_Id 
-					AND m.Type = 'Lecture'
-					AND DATEPART(MONTH, @DateInProcess) = DATEPART(MONTH, m.Begin_date)
-					AND DATEPART(YEAR, @DateInProcess) = DATEPART(YEAR, m.Begin_date)
-				JOIN [hurtownia].[dbo].t_employee e
-				ON m.FK_Employee_PESEL = e.PESEL
-		) lecturer
-		ON student.StudentID = lecturer.LecturerID
-		
-
-		DECLARE @MonthId int
-		SELECT TOP 1 
-			@MonthId = ID
-		FROM [hurtownia].[dbo].t_date
-		WHERE
-			DateYear = DATEPART(YEAR, @DateInProcess)
-			AND DateMonth = DATEPART(MONTH, @DateInProcess)
-			AND DateDay IS NULL	 
-		Set @DateInProcess = DateAdd(m, 1, @DateInProcess);
-		PRINT @MonthId
-	End
-	*/
+CREATE VIEW nullEmployee AS
+SELECT * 
+FROM [hurtownia].[dbo].t_employee 
+WHERE PESEL IS NULL AND EmployeeName IS NULL
+GO
 
 IF (object_id('etlCourseParticipationDrivesDone') is not null) DROP VIEW etlCourseParticipationDrivesDone
 GO
@@ -136,3 +43,103 @@ FROM
 	GROUP BY id_pesel_meetingId.ID, d.ID
 GO
 
+IF (object_id('etlInstructor') is not null) DROP VIEW etlInstructor
+GO
+
+CREATE VIEW etlInstructor AS
+SELECT
+	id_pesel_meetingId.ID AS [StudentID],
+	IIF(e.ID IS NULL, (SELECT TOP 1 ID FROM nullEmployee) ,e.ID) AS [InstructorID]
+FROM
+	(
+		SELECT ID, PESEL, FK_Meeting_Id FROM 
+		(
+			SELECT PK_PESEL
+			FROM [DrivingSchool16].[dbo].Student
+		) s
+		JOIN 
+		(
+			SELECT ID, PESEL
+			FROM [hurtownia].[dbo].t_student 
+		) ts
+		ON s.PK_PESEL = ts.PESEL
+		JOIN 
+		(
+			SELECT *
+			FROM [DrivingSchool16].[dbo].Participation
+		) p
+		ON s.PK_PESEL = p.FK_Student_PESEL
+		GROUP BY ts.ID, ts.PESEL, p.FK_Meeting_Id
+	) id_pesel_meetingId
+	JOIN
+		[DrivingSchool16].[dbo].Meeting m
+	ON id_pesel_meetingId.FK_Meeting_Id = m.PK_Id AND m.[Type] = 'Practice'
+	LEFT JOIN [hurtownia].[dbo].t_employee e
+	ON e.PESEL = m.[FK_Employee_PESEL]
+	GROUP BY id_pesel_meetingId.ID , e.ID
+GO
+
+IF (object_id('etlLecturer') is not null) DROP VIEW etlLecturer
+GO
+
+CREATE VIEW etlLecturer AS
+SELECT
+	id_pesel_meetingId.ID AS [StudentID],
+	IIF(e.ID IS NULL, (SELECT TOP 1 ID FROM nullEmployee) ,e.ID) AS [LecturerID]
+FROM
+	(
+		SELECT ID, PESEL, FK_Meeting_Id FROM 
+		(
+			SELECT PK_PESEL
+			FROM [DrivingSchool16].[dbo].Student
+		) s
+		JOIN 
+		(
+			SELECT ID, PESEL
+			FROM [hurtownia].[dbo].t_student 
+		) ts
+		ON s.PK_PESEL = ts.PESEL
+		JOIN 
+		(
+			SELECT *
+			FROM [DrivingSchool16].[dbo].Participation
+		) p
+		ON s.PK_PESEL = p.FK_Student_PESEL
+		GROUP BY ts.ID, ts.PESEL, p.FK_Meeting_Id
+	) id_pesel_meetingId
+	JOIN
+		[DrivingSchool16].[dbo].Meeting mt
+	ON id_pesel_meetingId.FK_Meeting_Id = mt.PK_Id AND mt.[Type] = 'Lecture'
+	LEFT JOIN [hurtownia].[dbo].t_employee e
+	ON e.PESEL = mt.[FK_Employee_PESEL]
+	GROUP BY id_pesel_meetingId.ID , e.ID
+GO
+
+Declare @StartID int; 
+Declare @EndID int;
+
+SELECT 
+	@StartID=MIN(MonthID), 
+	@EndID=MAX(MonthID)
+FROM
+	etlCourseParticipationDrivesDone d
+	LEFT JOIN etlInstructor i
+	ON d.StudentID = i.StudentID
+	LEFT JOIN etlLecturer l
+	ON d.StudentID = l.StudentID
+
+Declare @CurrentID int = @StartID;
+While @CurrentID <= @EndID
+	Begin
+		SELECT d.StudentID, InstructorID, LecturerID, MonthID, DrivesDone
+		FROM
+			etlCourseParticipationDrivesDone d
+			LEFT JOIN etlInstructor i
+			ON d.StudentID = i.StudentID
+			LEFT JOIN etlLecturer l
+			ON d.StudentID = l.StudentID
+		WHERE
+			d.MonthID = @CurrentID
+		SET @CurrentID = @CurrentID + 1
+	End
+GO
